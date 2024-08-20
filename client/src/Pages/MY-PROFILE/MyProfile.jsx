7935e7394c09ffb3bfd6a33e9230ea2464290daa
Serkan <s8ersis@gmail.com>
2024-08-20 18:14:00 +0300

    input  in profile page fixed for password part

--- client/src/Pages/MY-PROFILE/MyProfile.jsx
+++ client/src/Pages/MY-PROFILE/MyProfile.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import useAuthCalls from "../../Custom-hooks/useAuthCalls";
 import DOMPurify from "dompurify";
-import profileStyles from "./MyProfile.module.scss"
+import profileStyles from "./MyProfile.module.scss";
 import BlogPost from "../../Components/BLOG-POST/BlogPost";
 import QuillEditor from "../../Components/QUILL/QuillEditor";
 import { formRegisterInputs } from "../../Helpers/formInputs";
@@ -11,7 +11,7 @@ const MyProfile = () => {
   const { updatedUser } = useAuthCalls();
   const { user } = useSelector((state) => state.auth);
 
-console.log(user);
+  console.log(user);
   const inputRefs = useRef({
     username: user?.username,
     firstName: user?.firstName,
@@ -19,30 +19,34 @@ console.log(user);
     email: user?.email,
     image: user?.image || [],
     biography: user?.biography || "",
-    password: "" 
+    password: "",
   });
-  
+
   const [userModal, setUserModal] = useState(false);
   const quillRef = useRef(user?.biography);
-  
+
   const handleForm = (e) => {
     const { name, value } = e.target;
     inputRefs.current[name] = value;
   };
-// console.log(user);
+  // console.log(user);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const sanitizedContent = DOMPurify.sanitize(quillRef.current.getEditor().root.innerHTML, {
-      USE_PROFILES: { html: true },
-    });    const userInfo = {
+    const sanitizedContent = DOMPurify.sanitize(
+      quillRef.current.getEditor().root.innerHTML,
+      {
+        USE_PROFILES: { html: true },
+      }
+    );
+    const userInfo = {
       ...inputRefs.current,
       biography: sanitizedContent,
     };
     const userId = user?.id;
     updatedUser(userId, userInfo);
     // inputRefs.current = ""
-    setUserModal(false)
+    setUserModal(false);
   };
 
   return (
@@ -58,8 +62,7 @@ console.log(user);
         <h2>{user?.username}</h2>
       </section>
       <section className={profileStyles["profile-body"]}>
-        {/* <p>{user?.biography || "biography"}</p> */}
-        <BlogPost content={user?.biography}/>
+        <BlogPost content={user?.biography} />
       </section>
       <button onClick={() => setUserModal(!userModal)}>Edit Profile</button>
       <button>my blogs</button>
@@ -67,83 +70,29 @@ console.log(user);
       {userModal && (
         <div>
           <form onSubmit={handleSubmit}>
- {
-  formRegisterInputs.map(item => (
-    item.name !== "biography" &&
-    <React.Fragment key={item.name}>
-        <label htmlFor={item.name}>{item.label}</label>
-    <input
-      type={item.type}
-      id={item.name}
-      name={item.name}
-      defaultValue={inputRefs.current[item.name]}
-      onChange={handleForm}
-    />
-    </React.Fragment>
-  
-
-  ))
- }
-            {/* <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              defaultValue={inputRefs.current?.username}
-              onChange={handleForm}
-            />
-
-            <label htmlFor="firstName">First Name</label>
-            <input
-              type="text"
-              id="firstName"
-              name="firstName"
-              defaultValue={inputRefs.current?.firstName}
-              onChange={handleForm}
-            />
-
-            <label htmlFor="lastName">Last Name</label>
-            <input
-              type="text"
-              id="lastName"
-              name="lastName"
-              defaultValue={inputRefs.current?.lastName}
-              onChange={handleForm}
-            />
-
-            <label htmlFor="email">Email</label>
-            <input
-              type="text"
-              id="email"
-              name="email"
-              defaultValue={inputRefs.current?.email}
-              onChange={handleForm}
-            />
-
-
-            <label htmlFor="image">Image</label>
-            <input
-              type="text"
-              id="image"
-              name="image"
-              defaultValue={inputRefs.current?.image}
-              onChange={handleForm}
-            /> */}
+            {formRegisterInputs.map(
+              (item) =>
+                item.name !== "biography" && (
+                  <React.Fragment key={item.name}>
+                    <label htmlFor={item.name}>{item.label}</label>
+                    <input
+                      type={item.type}
+                      id={item.name}
+                      name={item.name}
+                      defaultValue={inputRefs.current[item.name]}
+                      onChange={handleForm}
+                      required={item.type === "password"}
+                    />
+                  </React.Fragment>
+                )
+            )}
 
             <label htmlFor="biography">Biography</label>
-            {/* <ReactQuill
-              className={profileStyles.quill}
-              id="biography"
-              theme="snow"
-              name="quill"
-              value={inputRefs.current.biography}
-              ref={quillRef}
-              modules={modules}
-            /> */}
             <QuillEditor
-            name="quill"
-            value={inputRefs.current?.biography}
-            ref={quillRef}
+              name="quill"
+              value={inputRefs.current?.biography}
+              ref={quillRef}
+              style={{ color: "black" }}
             />
 
             <button>Submit</button>
