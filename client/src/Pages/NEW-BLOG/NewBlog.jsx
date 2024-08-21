d9008318b51227019dfaca4ac3c7b594d937505e
Serkan <s8ersis@gmail.com>
2024-08-21 23:42:09 +0300

    new blog updated

--- client/src/Pages/NEW-BLOG/NewBlog.jsx
+++ client/src/Pages/NEW-BLOG/NewBlog.jsx
@@ -1,42 +1,12 @@
-import React, { useEffect, useRef, useState } from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
 import useAxios from "../../Custom-hooks/useAxios";
-import newBlogStyle from "./NewBlog.module.scss";
-import useBlogData from "../../Custom-hooks/useBlogData";
-import DOMPurify from "dompurify";
-import { useNavigate } from "react-router-dom";
-import { modules } from "../../Helpers/quillModules";
-import useDebounce from "../../Custom-hooks/useDebounce";
 import BlogModal from "../../Components/BLOG-MODAL/BlogModal";
-import QuillEditor from "../../Components/QUILL/QuillEditor";
+import style from "./NewBlog.module.scss";
 
 const NewBlog = () => {
-  const { categories } = useSelector((state) => state.blog);
-  const { getData } = useBlogData();
   const { axiosWithToken } = useAxios();
+  const [show, setShow] = useState(false)
 
-  const inputRefs = useRef({
-    title: "",
-    image: "",
-    categories: "",
-    isPublish: "",
-  });
-  const [text, setText] = useState("");
-  const [show, setShow] = useState(false);
-  const quillRef = useRef(null);
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    getData("categories");
-  }, []);
-
-  const handleForm = (e) => {
-    const { name, value } = e.target;
-    inputRefs.current[name] = value;
-  };
 
   const postBlog = async (url, postData) => {
     try {
@@ -47,25 +17,11 @@ const NewBlog = () => {
     }
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const sanitizedContent = DOMPurify.sanitize(quillRef.current.value, {
-      USE_PROFILES: { html: true },
-    });
-
-    const postData = {
-      ...inputRefs.current,
-      content: sanitizedContent,
-    };
-    console.log(postData);
-    await postBlog("blogs", postData);
-
-    navigate("/blogs");
-  };
+  
 
   return (
-    <section className={newBlogStyle["new-blog-main"]}>
-      <main className={newBlogStyle["form-container"]}>
+    <section className={style["new-blog-main"]}>
+      <main className={style["form-container"]}>
         <div>
           <BlogModal postBlog={postBlog} onClose={setShow} />
         </div>
